8669715a378270c32993140ba3c979c3a33a0ee8
igruk <88056536+igruk@users.noreply.github.com>
2023-04-25 02:08:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Developer Portfolio Website
+
+This is a developer portfolio website built using **HTML 5**, **CSS 3**, and **Bootstrap 5**. The map is created using **Leaflet 1.9**.
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Leaflet Map
+
+var map = L.map('map').setView([50.6159, 26.2500], 11);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
